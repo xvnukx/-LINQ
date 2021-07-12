c3e55849ef14a43fe309db8f2f3debce1344727f
Владислав Дыляев <xsinkinxsinkinx@gmail.com>
2021-07-12 22:46:26 +0300

    Добавьте файлы проекта.

---
+++ Практика LINQ/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_LINQ
+{
+    class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Практика LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+//Есть список учеников школы с разбивкой по классам:
+//Напишите метод, который соберет всех учеников всех классов в один список, используя LINQ.
+namespace Практика_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(s => s.Students).ToArray();
+        }
+    }
+}
